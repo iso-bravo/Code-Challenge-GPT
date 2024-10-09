06bc960e12a4c684ca083350b6902477f4f97eff
iso-bravo <isolettebravo@gmail.com>
2024-10-08 18:31:35 -0700

    init

---
+++ src/test/java/mx/edu/cetys/iso/chatgptapp/ChatgptappApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.cetys.iso.chatgptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
